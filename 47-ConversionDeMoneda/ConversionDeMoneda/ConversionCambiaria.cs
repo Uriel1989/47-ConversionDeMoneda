ee2fb4b5541a9cdac91299b6f80b1f633742ab30
Uriel Guillermo Daniel <45216219+Uriel1989@users.noreply.github.com>
2022-06-15 20:57:39 -0300

    Add files via upload

---
+++ 47-ConversionDeMoneda/ConversionDeMoneda/ConversionCambiaria.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConversionDeMoneda
+{
+    class ConversionCambiaria
+    {
+        public decimal PasajeDeMoneda (decimal MonedaNacional, decimal MonedaExtrangera)
+        {
+            return MonedaNacional / MonedaExtrangera;
+        }
+    }
+}
---
+++ 47-ConversionDeMoneda/ConversionDeMoneda/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConversionDeMoneda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenido al convertidor de moneda, ingrese el nombre de la moneda extrangera en cuestion para hacer el calculo:");
+
+            string NombreDeMoneda = Convert.ToString(Console.ReadLine());
+
+            Console.WriteLine("Ingrese la cantidad de pesos a convertir:");
+            decimal pesos = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el valor de la unidad de la moneda extragera en relacion a la moneda nacional:");
+            decimal monedaextrangera = Convert.ToDecimal(Console.ReadLine());
+
+            ConversionCambiaria conversionCambiaria = new ConversionCambiaria();
+
+            Console.WriteLine("Los pesos ingresados equivalen a: " + conversionCambiaria.PasajeDeMoneda(pesos, monedaextrangera)+" "+ NombreDeMoneda);
+
+            Console.ReadKey();
+        }
+    }
+}
